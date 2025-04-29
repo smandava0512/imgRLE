c3ce8a04811e59f3a7f991536b848712b75aa7f6
shrav <smandava1@ufl.edu>
2025-04-29 19:10:23 -0400

    dsflSDKJGK

--- Week12Lab_A.py
+++ Week12Lab_A.py
@@ -1,4 +1,21 @@
 import console_gfx
+def printMenu():
+    print("\nRLE Menu")
+    print("--------")
+    print("0. Exit")
+    print("1. Load File")
+    print("2. Load Test Image")
+    print("3. Read RLE String")
+    print("4. Read RLE Hex String")
+    print("5. Read Data Hex String")
+    print("6. Display Image")
+    print("7. Display RLE String")
+    print("8. Display Hex RLE Data")
+    print("9. Display Hex Flat Data")
+
+    option = input("\nSelect a Menu Option: ")
+
+    return option;
 
 def main():
     image_data = None
@@ -7,22 +24,11 @@ def main():
     print("Displaying Spectrum Image:")
     console_gfx.display_image(console_gfx.test_rainbow)
 
-    option = input("\nSelect a Menu Option: ")
+    option = 7
 
-    while option != 0:
-        print("\nRLE Menu")
-        print("--------")
-        print("0. Exit")
-        print("1. Load File")
-        print("2. Load Test Image")
-        print("3. Read RLE String")
-        print("4. Read RLE Hex String")
-        print("5. Read Data Hex String")
-        print("6. Display Image")
-        print("7. Display RLE String")
-        print("8. Display Hex RLE Data")
-        print("9. Display Hex Flat Data")
+    while option != '0':
 
+        option = printMenu()
 
         if option == '1':
             filename = input("Enter name of file to load: ")
@@ -41,6 +47,9 @@ def main():
                 console_gfx.display_image(image_data)
             else:
                 print("No image loaded.")
+
+        elif option == '0':
+            break
 
         else:
             print("Invalid option. Please choose again.")
